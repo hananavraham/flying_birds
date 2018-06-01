7d39fbe30196708372ecbd4f31e1e510cf0bebf1
Leon <leon.ben2892@gmail.com>
2018-06-01 13:41:01 +0300

    Added life graphics & losing lives logic to the game.

--- app/src/main/java/com/finalproject/hananavr/flying_birds/Game.java
+++ app/src/main/java/com/finalproject/hananavr/flying_birds/Game.java
@@ -5,6 +5,9 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Picture;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -16,24 +19,26 @@ public class Game extends View {
 
     ArrayList<leftBird> leftBirds = new ArrayList<leftBird>();
     ArrayList<rightBird> rightBirds = new ArrayList<rightBird>();
+    int lives;
 
 
     public Game(Context context) {
         super(context);
+        lives = 5;
+        //Random r = new Random();
         Bitmap b1 = BitmapFactory.decodeResource(getResources(), R.drawable.bird1_1);
         leftBird bird = new leftBird(context, b1);
-        //Random r = new Random();
-        bird.setBirdInfo(0, 0, 5);
+        bird.setBirdInfo(-260, 0, 5);
 
         leftBird leftBird = new leftBird(context,b1);
-        leftBird.setBirdInfo(0,500, 10);
+        leftBird.setBirdInfo(-260,500, 10);
         leftBirds.add(bird);
         leftBirds.add(leftBird);
 
 
         Bitmap b2 = BitmapFactory.decodeResource(getResources(), R.drawable.rightbird3);
         rightBird birdd = new rightBird(context,b2);
-        birdd.setBirdInfo(Resources.getSystem().getDisplayMetrics().widthPixels - 50,0, 8);
+        birdd.setBirdInfo(Resources.getSystem().getDisplayMetrics().widthPixels,0, 8);
         rightBirds.add(birdd);
 
     }
@@ -52,7 +57,40 @@ public class Game extends View {
             rightBird bird = (rightBird) it2.next();
             bird.draw(canvas);
         }
+
+        int size = getResources().getDimensionPixelSize(R.dimen.myFontSize);
+        Paint paint = new Paint();
+        paint.setColor(Color.BLACK);
+        paint.setTextSize(size);
+        canvas.drawText(String.valueOf(lives),10,size, paint);
+
+        Bitmap heart = BitmapFactory.decodeResource(getResources(), R.drawable.heart);
+        canvas.drawBitmap(heart,35,4,null);
+        checkBirdPassingScreen();
         invalidate();
+    }
+
+    private void checkBirdPassingScreen() {
+        Iterator it = leftBirds.iterator();
+        while (it.hasNext()) {
+            leftBird bir = (leftBird) it.next();
+            if(bir.getX() > Resources.getSystem().getDisplayMetrics().widthPixels){
+                lives--;
+                leftBirds.remove(bir);
+                return;
+            }
+        }
+
+        ListIterator it2 = rightBirds.listIterator();
+        while (it2.hasNext()){
+            rightBird bird = (rightBird) it2.next();
+            if(bird.getX()+bird.getImageWidth() < 0)
+            {
+                lives--;
+                rightBirds.remove(bird);
+                return;
+            }
+        }
     }
 
     public boolean onTouchEvent(MotionEvent event) {
@@ -61,7 +99,7 @@ public class Game extends View {
             float x = event.getX();
             float y = event.getY();
 
-            //Move through all RIGHT birds and check if the user clicked on any of the RIGHT birds or not 
+            //Move through all RIGHT birds and check if the user clicked on any of the RIGHT birds or not
             ListIterator it2 = rightBirds.listIterator();
             while (it2.hasNext()) {
                 rightBird bir2 = (rightBird) it2.next();
