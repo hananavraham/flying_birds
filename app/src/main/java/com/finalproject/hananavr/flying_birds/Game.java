264cfe9fa9c5e68293a93be6aeca4f2e27b682fd
Leon <leon.ben2892@gmail.com>
2018-06-02 23:17:52 +0300

    Refined some game logic, object creation and added multithreading for better game performance.

--- app/src/main/java/com/finalproject/hananavr/flying_birds/Game.java
+++ app/src/main/java/com/finalproject/hananavr/flying_birds/Game.java
@@ -23,53 +23,62 @@ public class Game extends View {
     ArrayList<leftBird> leftBirds = new ArrayList<>();
     ArrayList<rightBird> rightBirds = new ArrayList<>();
     Paint paint;
-    Bitmap heart;
-    Bitmap inGameMenu;
-    Bitmap rightShooter;
-    Bitmap leftShooter;
-    int pause_flg;
-    int shooterDirection_flg = 0;
-    int lives;
     InGameMenu igm;
+    Bitmap heart, inGameMenu, rightShooter, leftShooter, deadbird1, deadbird3;
+    int pause_flg, lives, fontSize, shooterDirection_flg;
+
 
     public Game(Context context) {
         super(context);
         lives = 5;
-        paint = new Paint();
         pause_flg = 0;
+        shooterDirection_flg = 0;
         //Random r = new Random();
         Bitmap b1 = BitmapFactory.decodeResource(getResources(), R.drawable.bird1_1);
         leftBird bird = new leftBird(context, b1);
         bird.setBirdInfo(-260, 0, 5);
+        leftBirds.add(bird);
 
         leftBird leftBird = new leftBird(context,b1);
         leftBird.setBirdInfo(-260,500, 10);
-        leftBirds.add(bird);
         leftBirds.add(leftBird);
-
 
         Bitmap b2 = BitmapFactory.decodeResource(getResources(), R.drawable.rightbird3);
         rightBird birdd = new rightBird(context,b2);
         birdd.setBirdInfo(Resources.getSystem().getDisplayMetrics().widthPixels,0, 8);
         rightBirds.add(birdd);
 
+        //Setting for font style (used for showing the live left as text)
+        fontSize = getResources().getDimensionPixelSize(R.dimen.myFontSize);
+        paint = new Paint();
+        paint.setColor(Color.BLACK);
+        paint.setTextSize(fontSize);
+
+        //All the necessery resources for in game menu
+        inGameMenu = BitmapFactory.decodeResource(getResources(), R.drawable.menu_button);
         Bitmap igmBackground = BitmapFactory.decodeResource(getResources(), R.drawable.igmbackground);
         Bitmap igmResume = BitmapFactory.decodeResource(getResources(), R.drawable.resumegameoption);
         Bitmap igmMainMenu = BitmapFactory.decodeResource(getResources(), R.drawable.mainmenuoption);
         Bitmap igmExit = BitmapFactory.decodeResource(getResources(), R.drawable.exitgameoption);
         igm = new InGameMenu(context,igmBackground,igmResume,igmMainMenu,igmExit);
 
+        //Heart image to show next to the lives left text
         heart = BitmapFactory.decodeResource(getResources(), R.drawable.heart);
-        inGameMenu = BitmapFactory.decodeResource(getResources(), R.drawable.menu_button);
 
+        //Pictures of left & right shooter direction for changing shooter direction animation
         rightShooter = BitmapFactory.decodeResource(getResources(), R.drawable.rightshooter);
         leftShooter = BitmapFactory.decodeResource(getResources(), R.drawable.leftshooter);
+
+        //Pictures of dead birds for dead bird animation
+        deadbird1 = BitmapFactory.decodeResource(getResources(), R.drawable.deadbird1);
+        deadbird3 = BitmapFactory.decodeResource(getResources(), R.drawable.deadbird3);
 
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
         Iterator it = leftBirds.iterator();
         while (it.hasNext()) {
             leftBird bir = (leftBird) it.next();
@@ -87,10 +96,7 @@ public class Game extends View {
         }
 
         //Drawing lives left as text
-        int size = getResources().getDimensionPixelSize(R.dimen.myFontSize);
-        paint.setColor(Color.BLACK);
-        paint.setTextSize(size);
-        canvas.drawText(String.valueOf(lives),10,size, paint);
+        canvas.drawText(String.valueOf(lives),10,fontSize, paint);
 
         //Drawing heart image
         canvas.drawBitmap(heart,35,4,null);
@@ -150,10 +156,9 @@ public class Game extends View {
                 ListIterator it2 = rightBirds.listIterator();
                 while (it2.hasNext()) {
                     rightBird bir2 = (rightBird) it2.next();
-                    if (x >= bir2.getX() && x < (bir2.getX()+bir2.getImageWidth()) && y >= bir2.getY() && y < (bir2.getY()+bir2.getImageHeight())) {
-                        if(!bir2.isDead())
+                    if (!bir2.isDead()) {
+                        if(x >= bir2.getX() && x < (bir2.getX()+bir2.getImageWidth()) && y >= bir2.getY() && y < (bir2.getY()+bir2.getImageHeight()))
                         {
-                            Bitmap deadbird3 = BitmapFactory.decodeResource(getResources(), R.drawable.deadbird3);
                             bir2.setImage(deadbird3);
                             bir2.setBirdSpeedY(10);
                             bir2.setDead(true);
@@ -166,10 +171,9 @@ public class Game extends View {
                 ListIterator it = leftBirds.listIterator();
                 while (it.hasNext()) {
                     leftBird bir = (leftBird) it.next();
-                    if (x >= bir.getX() && x < (bir.getX()+bir.getImageWidth()) && y >= bir.getY() && y < (bir.getY()+bir.getImageHeight())) {
-                        if(!bir.isDead())
+                    if (!bir.isDead()) {
+                        if(x >= bir.getX() && x < (bir.getX()+bir.getImageWidth()) && y >= bir.getY() && y < (bir.getY()+bir.getImageHeight()))
                         {
-                            Bitmap deadbird1 = BitmapFactory.decodeResource(getResources(), R.drawable.deadbird1);
                             bir.setImage(deadbird1);
                             bir.setBirdSpeedY(10);
                             bir.setDead(true);
@@ -180,16 +184,21 @@ public class Game extends View {
 
                 if(x >= Resources.getSystem().getDisplayMetrics().widthPixels-100 && x < (Resources.getSystem().getDisplayMetrics().widthPixels-100+inGameMenu.getWidth()) && y >= 4 && y < (4+inGameMenu.getHeight())){
                     pause_flg = 1;
-                    ListIterator it3 = leftBirds.listIterator();
-                    while (it3.hasNext()) {
-                        leftBird bir = (leftBird) it3.next();
-                        bir.pausedState();
-                    }
-                    ListIterator it4 = rightBirds.listIterator();
-                    while (it4.hasNext()) {
-                        rightBird bir2 = (rightBird) it4.next();
-                        bir2.pausedState();
-                    }
+                    Thread pausedStateThread = new Thread(){
+                        public void run(){
+                            ListIterator it3 = leftBirds.listIterator();
+                            while (it3.hasNext()) {
+                                leftBird bir = (leftBird) it3.next();
+                                bir.pausedState();
+                            }
+                            ListIterator it4 = rightBirds.listIterator();
+                            while (it4.hasNext()) {
+                                rightBird bir2 = (rightBird) it4.next();
+                                bir2.pausedState();
+                            }
+                        }
+                    };
+                    pausedStateThread.start();
                     return true;
                 }
             }
@@ -197,7 +206,7 @@ public class Game extends View {
             if(pause_flg == 1){
                 if(x >= 810 && x < (810+igm.getOptionWidth()) && y >= 265 && y < (265+igm.getOptionHeight()))
                 {
-                    Thread backToRunningStateThread = new Thread(){
+                    Thread runningStateThread = new Thread(){
                         public void run(){
                             ListIterator it = leftBirds.listIterator();
                             while (it.hasNext()) {
@@ -212,15 +221,29 @@ public class Game extends View {
                             }
                         }
                     };
-                    backToRunningStateThread.start();
+                    runningStateThread.start();
                     pause_flg = 0;
                 }
-                if(x >= 810 && x < (810+igm.getOptionWidth()) && y >= 350 && y < (350+igm.getOptionHeight()))
-                    getContext().startActivity(new Intent(this.getContext(),MainActivity.class));
-                if(x >= 810 && x < (810+igm.getOptionWidth()) && y >= 435 && y < (435+igm.getOptionHeight()))
-                    System.exit(0);
+                if(x >= 810 && x < (810+igm.getOptionWidth()) && y >= 350 && y < (350+igm.getOptionHeight())){
+                    Thread backToMainMenuThread = new Thread(){
+                        public void run(){
+                            getContext().startActivity(new Intent(getContext(),MainActivity.class));
+                        }
+                    };
+                    backToMainMenuThread.start();
+                    return true;
+                }
+                if(x >= 810 && x < (810+igm.getOptionWidth()) && y >= 435 && y < (435+igm.getOptionHeight())){
+                    Thread exitGameThread = new Thread(){
+                        public void run(){
+                            System.exit(0);
+                        }
+                    };
+                    exitGameThread.start();
+                    return true;
+                }
             }
-    }
+        }
         return false;
     }
 }
